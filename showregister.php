416ebef0a2334d4aa3700c7ba5c108659e6516f6
phongpakon35 <phongpakon@hotmail.com>
2015-06-09 22:11:27 +0700

    เพิ่มไฟล์ showregister.php

---
+++ showregister.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+</head>
+<center>
+<body bgcolor="#6699FF">
+<h1>ระบบสมัครสมาชิก</h1><br>
+
+
+<fieldset style="width:700px;height:500px;">
+<legend>
+กรุณากรอกข้อมูลให้ครบถ้วน:
+</legend>
+		<form name="frmMain" method="post" action="">
+
+<?php 
+if(count($error) > 0)
+{
+	foreach($error as $key => $value)
+	{
+		echo $value . '<br>';
+	}
+
+}
+
+	if(count($suscess) > 0)
+{
+	foreach($suscess as $key => $value)
+	{
+		echo $value . '<br>';
+	}
+}
+	
+?>
+
+
+<br/> <br/> <b>ชื่อ : </b> <input type="text" name="name" placeholder="ชื่อ" style="background: #FFF0F5;" size="30">
+<b>นามสกุล :   </b> <input type="text" name="lastnmae" placeholder="นามสกุล"style="background: #FFF0F5" size="30"><br/> <br/> <br/> 
+
+															
+<b> User : </b> <input type="text" name="user" placeholder="รหัสนักศึกษา" style="background: #FFF0F5" maxlength=9 size="30">
+<b> Password : </b> <input type="password" placeholder="วัน/เดือน/ปีเกิด **ค.ศ "style="background: #FFF0F5" maxlength=8 name=user size="30"> <br /><br /> <br /> 
+
+<input type="submit" value="สมัครสมาชิก"><br /><br />
+</form>
+
+
+
+															 
+	
+
+
+
+</fieldset>
+
+
+</body>
+</center>
+</html>
